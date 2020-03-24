531e74cfb696250c60d1cb8f5693dc3a13850580
William Pratt <willpratt78@gmail.com>
2020-03-24 16:11:32 -0700

    chang to before and after instead of prepend

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
-    $("ul#user").prepend("<li>Meow</li>");
-    $("ul#webpage").append("<li>Bark</li>");
+    $("ul#user").after("<li>Meow</li>");
+    $("ul#webpage").after("<li>Bark</li>");
   });
 
   $("button#dog").click(function() {
-    $("ul#user").prepend("<li>Bark</li>");
-    $("ul#webpage").append("<li>Meow</li>");
+    $("ul#user").before("<li>Bark</li>");
+    $("ul#webpage").before("<li>Meow</li>");
   });
 
 });
